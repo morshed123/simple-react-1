51f55480001bbb0a21bea4d23ed42e1fdeb23a70
Your Name <you@example.com>
2021-05-05 06:56:14 +0600

    Use to fake Data

--- src/App.js
+++ src/App.js
@@ -2,20 +2,27 @@ import React, { useEffect, useState } from 'react';
 import './App.css';
 import Header from './Components/PlayerInfo/Header/Header';
 import Player from './Components/PlayerInfo/Player';
-import Total from './Components/PlayerInfo/TotalBudget/Total'
+import Total from './Components/PlayerInfo/TotalBudget/Total';
+import fakeData from './fakeData/data.json';
 
 function App() {
   const [players, setPlayers] = useState([]);
   const [playerCart, setPlayerCart] = useState([]);
 
   useEffect(() => {
-    // const url ='https://6085d2e2d14a870017578555.mockapi.io/api/v1/players'
-    const url ='https://608d6ad5fe2e9c00171e1a73.mockapi.io/api/v1/players'
-    fetch(url)
-    .then((response) =>response.json())
-    .then(data => setPlayers(data))
-    .catch(error => console.log(error));
+      setPlayers(fakeData)
+
   },[])
+
+  //// Or,  REST API URL use kore korte paro
+  // useEffect(() => {
+  //   // const url ='https://6085d2e2d14a870017578555.mockapi.io/api/v1/players'
+  //   const url ='https://608d6ad5fe2e9c00171e1a73.mockapi.io/api/v1/players'
+  //   fetch(url)
+  //   .then((response) =>response.json())
+  //   .then(data => setPlayers(data))
+  //   .catch(error => console.log(error));
+  // },[])
 
     const handleAddPlayer = (player) => {
     const newCart = [...playerCart, player];
